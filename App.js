ec631b63bf42dbc7df7215eb7e1e79746aecc1b9
Sohail <thisemailitakenbysohail@gmail.com>
2024-02-08 15:12:01 +0530

    final

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ let users=[];
 //adding user
 function addUser(socketId,userId,fullName)
 {
-    !users.some((user)=>user.userId==userId)&&users.push({socketId,userId,fullName})
+    !users.some((user)=>user._id==userId)&&users.push({socketId,_id:userId,fullName})
 }
 
 //removing a user
@@ -30,24 +30,32 @@ function removeUser(socketId)
 
 function findSocketId(userId)
 {
-    return users.find((user)=>user.userId===userId);
+    // console.log("to be find is ",userId)
+    return users.find((user)=>user._id===userId);
 }
 
 
 io.on("connection",(socket)=>{
-    console.log("A user is connected",socket.id);
+    // console.log("A user is connected",socket.id);
 
     socket.on("addUser",(userId,fullName)=>{
-        console.log("User id is ",userId,fullName)
+        // console.log("User id is ",userId,fullName)
               addUser(socket.id,userId,fullName);
               io.emit("getUsers",users);
               
     })
 
+    socket.on("saveMSG",async(data)=>{
+        // console.log("Data is ",data);
+        const soc=await findSocketId(data.receiverId);
+        // console.log("Socket id of receiver is ",soc)
+        io.to(soc?.socketId).emit("receiveMSG",data);
+    })
+
 
     //
     socket.on("disconnect",()=>{
-        console.log("A user is disconnected")
+        // console.log("A user is disconnected")
         removeUser(socket.id);
         io.emit("getUsers",users);
     })
